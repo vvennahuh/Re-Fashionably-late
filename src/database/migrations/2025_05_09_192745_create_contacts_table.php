ce898fb23e22b89a725a4e6a525756736568e6c1
vvennqhuh <vvennq@gmail.com>
2025-05-10 20:42:36 +0900

    Fortify導入

--- src/database/migrations/2025_05_09_192745_create_contacts_table.php
+++ src/database/migrations/2025_05_09_192745_create_contacts_table.php
@@ -15,6 +15,15 @@ class CreateContactsTable extends Migration
     {
         Schema::create('contacts', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('category_id')->constrained()->cascadeOnDelete(); // カテゴリIDを外部キーとして設定
+            $table->string('first_name'); // 名字
+            $table->string('last_name'); // 名前
+            $table->unsignedTinyInteger('gender'); // 性別
+            $table->string('email'); // メールアドレス
+            $table->string('tel'); // 電話番号
+            $table->string('address'); // 住所
+            $table->string('building')->nullable(); // 建物名
+            $table->text('detail'); // お問い合わせ内容
             $table->timestamps();
         });
     }
